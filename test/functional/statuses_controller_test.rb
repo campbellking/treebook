08b3057923dc0d06997c1568a31cb67d454b485c
campbellking <ck@campbellking.com.au>
2012-10-27 18:42:33 +1100

    Added tests for custom routes

--- test/functional/statuses_controller_test.rb
+++ test/functional/statuses_controller_test.rb
@@ -11,15 +11,22 @@ class StatusesControllerTest < ActionController::TestCase
     assert_not_nil assigns(:statuses)
   end
 
-  test "should get new" do
+  test "should be redirected when not logged in" do
+    get :new
+    assert_response :redirect
+    assert_redirected_to new_user_session_path
+  end
+
+  test "should render the new page when logged in" do
+    sign_in users(:campbell)
     get :new
     assert_response :success
   end
 
   test "should create status" do
     assert_difference('Status.count') do
-      post :create, status: { content: @status.content, name: @status.name }
-    end
+      post :create, status: { content: @status.content }
+  end
 
     assert_redirected_to status_path(assigns(:status))
   end
@@ -35,14 +42,14 @@ class StatusesControllerTest < ActionController::TestCase
   end
 
   test "should update status" do
-    put :update, id: @status, status: { content: @status.content, name: @status.name }
+    put :update, id: @status, status: { content: @status.content }
     assert_redirected_to status_path(assigns(:status))
   end
 
   test "should destroy status" do
     assert_difference('Status.count', -1) do
       delete :destroy, id: @status
-    end
+  end
 
     assert_redirected_to statuses_path
   end
